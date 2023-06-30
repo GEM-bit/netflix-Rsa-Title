1ac56291e111a2d56f63c30207abaf9b7bb0174a
GEM-bit <71544961+GEM-bit@users.noreply.github.com>
2023-06-30 09:47:49 +0200

    Included delimiter in Google file csv_read

--- netflixRecommend.py
+++ netflixRecommend.py
@@ -23,7 +23,7 @@ st.header(':red[Netflix South African Recomender] :film_projector:')
 def load_data():
     
     file_url = 'https://drive.google.com/uc?id=1rU1iKFZVfQ4GLbgc7Dozs8N4Tw7YTkU4'
-    df_embed = pd.read_csv(file_url)
+    df_embed = pd.read_csv(file_url,delimiter=',')
     #df_embed=pd.read_csv('netflixRsaEmbeddings.csv',delimiter=',')
     df_embed['embedding']=df_embed['embedding'].apply(eval).apply(np.array) #converts embedding to numpy array
     return df_embed
